68d01619c6d2cae778df46285b4c7d0873fa1151
OodavidsinoO <david.sin@tuta.io>
2023-06-21 21:53:16 +0800

    Update custom bundles' links

--- src/load_bundles.sh
+++
@@ -1,4 +0,0 @@
-# Copy bundles from the ../bundles to the ./moneros_overlay/bundles
-
-echo "Copying bundles from the '../bundles' to the './moneros_overlay/bundles'..."
-cp -f ../bundles/* ./moneros_overlay/bundles
